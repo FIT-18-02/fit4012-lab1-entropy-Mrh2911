7342f4b71899a1c0a9a7f2518cd3fb876990586b
Phạm Huy Hoàng <tigerhell641@gmail.com>
2026-04-15 15:17:47 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-
 using namespace std;
-
 int gcd(int a, int b) {
     while (b != 0) {
         int temp = b;
@@ -10,7 +8,6 @@ int gcd(int a, int b) {
     }
     return a;
 }
-
 int extended_euclid(int a, int b, int &x, int &y) {
     if (b == 0) {
         x = 1;
@@ -23,14 +20,12 @@ int extended_euclid(int a, int b, int &x, int &y) {
     y = x1 - (a / b) * y1;
     return g;
 }
-
 int mod_inverse(int a, int m) {
     int x, y;
     int g = extended_euclid(a, m, x, y);
     if (g != 1) return -1;
     return (x % m + m) % m;
 }
-
 int main() {
     int a, m;
     while (cin >> a >> m) {
