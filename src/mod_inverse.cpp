ee622356454b00ca2ca7502e1d8c470ea2609314
Phạm Huy Hoàng <tigerhell641@gmail.com>
2026-04-15 15:35:48 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -2,15 +2,6 @@
 
 using namespace std;
 
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
 int extended_euclid(int a, int b, int &x, int &y) {
     if (b == 0) {
         x = 1;
