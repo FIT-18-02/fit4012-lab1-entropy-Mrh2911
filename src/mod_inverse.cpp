f18e6db5a2553ac497306fa086921cb8fc284c4c
Phạm Huy Hoàng <tigerhell641@gmail.com>
2026-04-15 15:15:59 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -17,8 +17,7 @@ int extended_euclid(int a, int b, int &x, int &y) {
         y = 0;
         return a;
     }
-
-    int x1 = 0, y1 = 0;
+    int x1, y1;
     int g = extended_euclid(b, a % b, x1, y1);
     x = y1;
     y = x1 - (a / b) * y1;
@@ -28,25 +27,19 @@ int extended_euclid(int a, int b, int &x, int &y) {
 int mod_inverse(int a, int m) {
     int x, y;
     int g = extended_euclid(a, m, x, y);
-    
-    if (g != 1) {
-        return -1;
-    }
-
+    if (g != 1) return -1;
     return (x % m + m) % m;
 }
 
 int main() {
-    int a = 0, m = 0;
-    if (!(cin >> a >> m)) return 0;
-
-    int inv = mod_inverse(a, m);
-    
-    if (inv == -1) {
-        cout << "Khong ton tai nghich dao modulo vi gcd(a, m) != 1." << endl;
-    } else {
-        cout << inv << endl;
+    int a, m;
+    while (cin >> a >> m) {
+        int inv = mod_inverse(a, m);
+        if (inv == -1) {
+            cout << "Khong ton tai nghich dao modulo vi gcd(a, m) != 1." << endl;
+        } else {
+            cout << inv << endl;
+        }
     }
-    
     return 0;
 }
