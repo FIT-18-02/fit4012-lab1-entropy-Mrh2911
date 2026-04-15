502d9c0ab82760c435fe79491be5e867726a4f6e
Phạm Huy Hoàng <tigerhell641@gmail.com>
2026-04-15 15:13:53 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-// Ham tim uoc chung lon nhat
 int gcd(int a, int b) {
     while (b != 0) {
         int temp = b;
@@ -12,7 +11,6 @@ int gcd(int a, int b) {
     return a;
 }
 
-// Ham Euclid mo rong de tim cac he so x, y
 int extended_euclid(int a, int b, int &x, int &y) {
     if (b == 0) {
         x = 1;
@@ -27,23 +25,19 @@ int extended_euclid(int a, int b, int &x, int &y) {
     return g;
 }
 
-// Ham tinh nghich dao modulo da duoc hoan thien dứt điểm
 int mod_inverse(int a, int m) {
     int x, y;
     int g = extended_euclid(a, m, x, y);
     
-    // Neu gcd khac 1 thi khong ton tai nghich dao
     if (g != 1) {
         return -1;
     }
 
-    // Dua x ve mien duong [0, m-1]
     return (x % m + m) % m;
 }
 
 int main() {
     int a = 0, m = 0;
-    // Doc du lieu tu cin
     if (!(cin >> a >> m)) return 0;
 
     int inv = mod_inverse(a, m);
